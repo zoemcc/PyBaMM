52a55eb387f79b058fdae130b8cae648b4486f2f
Ivan Korotkin <i.korotkin@soton.ac.uk>
2019-07-03 15:07:58 +0100

    Add daecpp_dae_solver draft. Update dae-cpp solver to the latest version.

--- pybamm/solvers/daecpp_dae_solver.py
+++ pybamm/solvers/daecpp_dae_solver.py
@@ -6,7 +6,7 @@ import pybamm
 
 import numpy as np
 import pydae
-import scipy.sparse as sparse
+#import scipy.sparse as sparse
 
 
 class DaecppDaeSolver(pybamm.DaeSolver):
@@ -56,50 +56,134 @@ class DaecppDaeSolver(pybamm.DaeSolver):
             (see `SUNDIALS docs. <https://computation.llnl.gov/projects/sundials>`).
         """
 
-        def eqsres(t, y, ydot, return_residuals):
-            return_residuals[:] = residuals(t, y, ydot)
+        # dae-cpp RHS
+        def fun_rhs(x, f, t):
+            # solver works with ydot set to zero
+            #y = np.array(x)
+            #ydot = np.zeros_like(y)
+            #f[:] = residuals(t, y, ydot)
+            f = pydae.state_type(residuals(t, x))
 
-        def rootfn(t, y, ydot, return_root):
-            return_root[:] = [event(t, y) for event in events]
+        # dae-cpp Mass Matrix
+        # TODO: Currently returns identity matrix of size y0.size.
+        # In general it should convert mass_matrix or return identity matrix if
+        # mass_matrix=None
+        def fun_mass_matrix(M):
+            size = y0.size
+            M.A.resize(size)
+            M.ja.resize(size)
+            M.ia.resize(size + 1)
+            for i in range(0, size):
+                # Non-zero and/or diagonal elements
+                M.A[i] = 1
+                # Column index of each element given above
+                M.ja[i] = i
+                # Index of the first element for each row
+                M.ia[i] = i
+            # To close the matrix
+            M.ia[size] = size
 
-        extra_options = {"old_api": False, "rtol": self.tol, "atol": self.tol}
+        #def eqsres(t, y, ydot, return_residuals):
+        #    return_residuals[:] = residuals(t, y, ydot)
+
+        #def rootfn(t, y, ydot, return_root):
+        #    return_root[:] = [event(t, y) for event in events]
+
+        #extra_options = {"old_api": False, "rtol": self.tol, "atol": self.tol}
+
+        # dae-cpp solver option
+        opt = pydae.SolverOptions()
+        opt.atol = self.tol
+        #opt.verbosity = 1
+        #opt.dt_init = 0.001
+        opt.time_stepping = 1
+
+        dae_mass = pydae.MassMatrix(fun_mass_matrix)
+        dae_rhs = pydae.RHS(fun_rhs)
 
         if jacobian:
-            jac_y0_t0 = jacobian(t_eval[0], y0)
-            if sparse.issparse(jac_y0_t0):
+            # TEMPORARY for testing
+            dae_jacobian = pydae.NumericalJacobian(dae_rhs, self.tol)
+            #raise NotImplementedError
+            
+            #jac_y0_t0 = jacobian(t_eval[0], y0)
+            #if sparse.issparse(jac_y0_t0):
 
-                def jacfn(t, y, ydot, residuals, cj, J):
-                    jac_eval = jacobian(t, y) - cj * mass_matrix
-                    J[:][:] = jac_eval.toarray()
+            #    def jacfn(t, y, ydot, residuals, cj, J):
+            #        jac_eval = jacobian(t, y) - cj * mass_matrix
+            #        J[:][:] = jac_eval.toarray()
 
-            else:
+            #else:
 
-                def jacfn(t, y, ydot, residuals, cj, J):
-                    jac_eval = jacobian(t, y) - cj * mass_matrix
-                    J[:][:] = jac_eval
+            #    def jacfn(t, y, ydot, residuals, cj, J):
+            #        jac_eval = jacobian(t, y) - cj * mass_matrix
+            #        J[:][:] = jac_eval
 
-            extra_options.update({"jacfn": jacfn})
+            #extra_options.update({"jacfn": jacfn})
+        else:
+            dae_jacobian = pydae.NumericalJacobian(dae_rhs, self.tol)
 
-        if events:
-            extra_options.update({"rootfn": rootfn, "nr_rootfns": len(events)})
+        #if events:
+        #    extra_options.update({"rootfn": rootfn, "nr_rootfns": len(events)})
 
         # solver works with ydot0 set to zero
-        ydot0 = np.zeros_like(y0)
+        #ydot0 = np.zeros_like(y0)
 
-        # set up and solve
-        dae_solver = scikits_odes.dae(self.method, eqsres, **extra_options)
-        sol = dae_solver.solve(t_eval, y0, ydot0)
+        # set the solver up
+        dae_solve = pydae.Solver(dae_rhs, dae_jacobian, dae_mass, opt)
+
+        # initial condition
+        x = pydae.state_type(y0)
+
+        # solve
+        status = 0
+        first_pass = True
+        for t1 in t_eval:
+            # currently t1 cannot be 0 for dae-cpp. TODO: remove this restriction
+            if(t1 == 0):
+                y_sol = np.reshape(np.array(x), (-1, 1))
+                t_sol = np.array([0])
+                first_pass = False
+                continue
+            else:
+                # solution for time t1
+                status += dae_solve(x, t1)  # x will be overwritten
+                sol_t1 = np.reshape(np.array(x), (-1, 1))
+            
+            if(first_pass):
+                y_sol = sol_t1
+                t_sol = np.array([t1])
+                first_pass = False
+            else:
+                y_sol = np.append(y_sol, sol_t1, axis=1)
+                t_sol = np.append(t_sol, t1)
+            print("###### t1, x: " + str(t1) + "  " + str(x))
+
+        #dae_solver = scikits_odes.dae(self.method, eqsres, **extra_options)
+        #sol = dae_solver.solve(t_eval, y0, ydot0)
+
+        # return solution
+        if status == 0:
+            # success
+            termination = "final time"
+            #return pybamm.Solution(
+            #    t_sol, np.transpose(y_sol), termination
+            #)
+            return pybamm.Solution(t_sol, y_sol, termination)
+        else:
+            # error
+            raise pybamm.SolverError("dae-cpp: Error during integration")
 
         # return solution, we need to tranpose y to match scipy's interface
-        if sol.flag in [0, 2]:
+        #if sol.flag in [0, 2]:
             # 0 = solved for all t_eval
-            if sol.flag == 0:
-                termination = "final time"
+        #    if sol.flag == 0:
+        #        termination = "final time"
             # 2 = found root(s)
-            elif sol.flag == 2:
-                termination = "event"
-            return pybamm.Solution(
-                sol.values.t, np.transpose(sol.values.y), termination
-            )
-        else:
-            raise pybamm.SolverError(sol.message)
+        #    elif sol.flag == 2:
+        #        termination = "event"
+        #    return pybamm.Solution(
+        #        sol.values.t, np.transpose(sol.values.y), termination
+        #    )
+        #else:
+        #    raise pybamm.SolverError(sol.message)
