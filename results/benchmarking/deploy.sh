a46463a76e60e95e1995f4dce43fa03224accc52
Travis CI <travis@travis-ci.org>
2019-10-25 20:51:42 -0400

    #513 trying new approach

--- results/benchmarking/deploy.sh
+++ results/benchmarking/deploy.sh
@@ -2,11 +2,32 @@
 # Benchmark solvers and push results to GitHub
 #
 
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_benchmark_files() {
+  git checkout master
+  # Current month and year, e.g: Apr 2018
+  dateAndMonth=`date "%F"`
+  # Stage the modified files in results 
+  git add results/benchmarking/README.md
+  # Create a new commit with a custom build message
+  # with "[skip ci]" to avoid a build loop
+  git commit -m "Travis benchmarking: $dateAndMonth" -m "[skip ci]"
+}
+
+upload_files() {
+  # Remove existing "origin"
+  git remote rm origin
+  # Add new "origin" with access token in the git URL for authentication
+  git remote add origin https://${GITHUB_TOKEN}@github.com/pybamm-team/pybamm.git > /dev/null 2>&1
+  git push origin master --quiet
+}
+
 # Benchmark
 python3 results/benchmarking/time_models_and_solvers.py
-
-# Push
-git checkout -b gh-pages
-git add results/benchmarking/README.md
-git commit -m "Travis CI: benchmark solvers"
-git push --quiet --set-upstream origin gh-pages
+setup_git
+commit_benchmark_files
+upload_files
