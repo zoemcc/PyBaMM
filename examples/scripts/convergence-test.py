c3fe97373952b4cbc58980d22968fb8f9ccd2702
Scott Marquis <marquis@maths.ox.ac.uk>
2020-03-06 12:10:40 +0000

    more compare

--- examples/scripts/convergence-test.py
+++ examples/scripts/convergence-test.py
@@ -4,24 +4,43 @@ import pickle
 import matplotlib.pyplot as plt
 
 
+pybamm.set_logging_level("INFO")
+
+
 run = False
+# filename = "compare_electrolyte.p"
+filename = "compare_particles.p"
 
-sol = pickle.load(open("compare.p", "rb"))
+if run is False:
+    sol = pickle.load(open(filename, "rb"))
 
-for i, pts in enumerate(sol["pts"]):
-    print("The error at ", pts, "pts is ", sol["errors"][i])
+    for i, pts in enumerate(sol["pts"]):
+        print("The error at ", pts, "pts is ", sol["errors"][i])
 
-plt.loglog(sol["pts"], sol["errors"])
-plt.loglog(
-    sol["pts"],
-    (sol["pts"][0] ** 2 / sol["errors"][0]) / sol["pts"] ** 2,
-    linestyle="--",
-)
-# plt.loglog(sol["pts"], 1 / sol["pts"] ** 2, linestyle=":")
-plt.show()
+    # plt.loglog(sol["pts"], sol["errors"])
+    plt.plot(sol["pts"], sol["errors"])
+    # plt.loglog(
+    #     sol["pts"],
+    #     (sol["pts"][0] ** 2 / sol["errors"][0]) / sol["pts"] ** 2,
+    #     linestyle="--",
+    # )
+    # plt.loglog(sol["pts"], 1 / sol["pts"] ** 2, linestyle=":")
+    plt.show()
 
 
 def pts(n):
+    var = pybamm.standard_spatial_vars
+    var_pts = {
+        var.x_n: 200,
+        var.x_s: 200,
+        var.x_p: 200,
+        var.r_n: n,
+        var.r_p: n,
+    }
+    return var_pts
+
+
+def pts_all(n):
     var = pybamm.standard_spatial_vars
     var_pts = {
         var.x_n: n,
@@ -33,12 +52,12 @@ def pts(n):
     return var_pts
 
 
-if run == True:
+if run is True:
     t_eval = np.linspace(0, 3300, 100)
     solver = pybamm.CasadiSolver(mode="fast")
 
     model_truth = pybamm.lithium_ion.DFN()
-    sim_truth = pybamm.Simulation(model_truth, var_pts=pts(70), solver=solver)
+    sim_truth = pybamm.Simulation(model_truth, var_pts=pts_all(200), solver=solver)
     sim_truth.solve()
 
     t_max = sim_truth.solution.t[-1]
@@ -46,7 +65,7 @@ if run == True:
     truth_voltage = sim_truth.solution["Terminal voltage [V]"](t)
 
     # other_pts = np.array([3, 5, 10, 30, 40])
-    other_pts = np.arange(3, 70, 1)
+    other_pts = np.arange(3, 70, 5)
     solutions = np.zeros_like(other_pts)
     errors = np.linspace(0, 1, len(other_pts))
 
@@ -59,9 +78,9 @@ if run == True:
         v = sim.solution["Terminal voltage [V]"](t)
         errors[i] = np.sum((v - truth_voltage) ** 2) ** (1 / 2)
 
+    out = {"pts": other_pts, "errors": errors}
+    pickle.dump(out, open(filename, "wb"))
+
     plt.loglog(other_pts, errors)
     plt.show()
-
-    out = {"pts": other_pts, "errors": errors}
-    pickle.dump(out, open("compare2.p", "wb"))
 
--- examples/scripts/spme-convergence.py
+++ examples/scripts/spme-convergence.py
@@ -3,57 +3,138 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 
+pybamm.set_logging_level("INFO")
 
-def pts(n):
-    var = pybamm.standard_spatial_vars
-    var_pts = {
-        var.x_n: n,
-        var.x_s: n,
-        var.x_p: n,
-        var.r_n: n,
-        var.r_p: n,
-        var.y: n,
-        var.z: n,
+run = False
+
+
+if run is False:
+
+    sol = pickle.load(open("spme_dfn_errors.p", "rb"))
+
+    plt.plot(sol["pts"], sol["voltage errors"])
+    plt.plot(sol["pts"], sol["c_s_n_errors"])
+    plt.plot(sol["pts"], sol["c_s_p_errors"])
+    plt.plot(sol["pts"], sol["phi_s_n_errors"])
+    plt.legend(["Voltage", "c_s_n", "c_s_p", "phi_s_n"])
+    plt.show()
+
+    # relative to final point
+    def rel(a):
+        return np.abs(a - a[-1]) / a[-1]
+
+    plt.loglog(sol["pts"], rel(sol["voltage errors"]))
+    plt.loglog(sol["pts"], rel(sol["c_s_n_errors"]))
+    plt.loglog(sol["pts"], rel(sol["c_s_p_errors"]))
+    plt.loglog(sol["pts"], rel(sol["phi_s_n_errors"]))
+    plt.legend(["Voltage", "c_s_n", "c_s_p", "phi_s_n"])
+    plt.show()
+else:
+
+    def pts(n):
+        var = pybamm.standard_spatial_vars
+        var_pts = {
+            var.x_n: n,
+            var.x_s: n,
+            var.x_p: n,
+            var.r_n: n,
+            var.r_p: n,
+            var.y: n,
+            var.z: n,
+        }
+        return var_pts
+
+    t_eval = np.linspace(0, 3300, 100)
+    solver = pybamm.CasadiSolver(mode="fast")
+
+    options = {}
+
+    model_truth = pybamm.lithium_ion.DFN(options)
+    sim_truth = pybamm.Simulation(model_truth, var_pts=pts(3), solver=solver)
+    sim_truth.solve()
+
+    t_max = sim_truth.solution.t[-1]
+    t = np.linspace(0, t_max, 100)
+    truth_voltage = sim_truth.solution["Terminal voltage [V]"](t)
+
+    other_pts = np.array([3, 5, 10, 20, 30, 40, 50, 100, 200])
+    solutions = np.zeros_like(other_pts)
+    voltage_errors = np.linspace(0, 1, len(other_pts))
+    c_s_n_errors = np.linspace(0, 1, len(other_pts))
+    c_s_p_errors = np.linspace(0, 1, len(other_pts))
+    phi_s_n_errors = np.linspace(0, 1, len(other_pts))
+
+    l_n = sim_truth.parameter_values.process_symbol(
+        pybamm.geometric_parameters.l_n
+    ).evaluate()
+    l_s = sim_truth.parameter_values.process_symbol(
+        pybamm.geometric_parameters.l_s
+    ).evaluate()
+    l_p = sim_truth.parameter_values.process_symbol(
+        pybamm.geometric_parameters.l_p
+    ).evaluate()
+
+    x_n = np.linspace(0, l_n, 100)
+    x_s = np.linspace(l_n, l_n + l_s, 100)
+    x_p = np.linspace(l_n + l_s, 1, 100)
+
+    for i, p in enumerate(other_pts):
+
+        dfn = pybamm.lithium_ion.DFN(options)
+        dfn_solver = pybamm.CasadiSolver(mode="fast")
+        dfn_sim = pybamm.Simulation(dfn, var_pts=pts(p), solver=solver)
+        dfn_sim.solve()
+
+        spme = pybamm.lithium_ion.SPMe(options)
+        spme_solver = pybamm.CasadiSolver(mode="fast")
+        spme_sim = pybamm.Simulation(spme, var_pts=pts(p), solver=solver)
+        spme_sim.solve()
+
+        v_dfn = dfn_sim.solution["Terminal voltage [V]"](t)
+        c_s_n = dfn_sim.solution["Negative particle surface concentration"](
+            t=t, x=x_n
+        ).flatten()
+        c_s_p = dfn_sim.solution["Positive particle surface concentration"](
+            t=t, x=x_p
+        ).flatten()
+        phi_s_n = dfn_sim.solution["Negative electrode potential [V]"](
+            t=t, x=x_n
+        ).flatten()
+
+        v_spm = spme_sim.solution["Terminal voltage [V]"](t)
+        c_s_n_spm = spme_sim.solution["Negative particle surface concentration"](
+            t=t, x=x_n
+        ).flatten()
+        c_s_p_spm = spme_sim.solution["Positive particle surface concentration"](
+            t=t, x=x_p
+        ).flatten()
+        phi_s_n_spm = spme_sim.solution["Negative electrode potential [V]"](
+            t=t, x=x_n
+        ).flatten()
+
+        def err(a, b):
+            norm = np.sum((a) ** 2) ** (1 / 2)  # normalizing factor
+            rms = np.sum((a - b) ** 2) ** (1 / 2)
+            return rms / norm
+
+        voltage_errors[i] = err(v_dfn, v_spm)
+        c_s_n_errors[i] = err(c_s_n, c_s_n_spm)
+        c_s_p_errors[i] = err(c_s_p, c_s_p_spm)
+        phi_s_n_errors[i] = err(phi_s_n, phi_s_n_spm)
+
+    out = {
+        "pts": other_pts,
+        "voltage errors": voltage_errors,
+        "c_s_n_errors": c_s_n_errors,
+        "c_s_p_errors": c_s_p_errors,
+        "phi_s_n_errors": phi_s_n_errors,
     }
-    return var_pts
+    pickle.dump(out, open("spme_dfn_errors.p", "wb"))
 
-
-t_eval = np.linspace(0, 3300, 100)
-solver = pybamm.CasadiSolver(mode="fast")
-
-options = {}
-
-model_truth = pybamm.lithium_ion.DFN(options)
-sim_truth = pybamm.Simulation(model_truth, var_pts=pts(70), solver=solver)
-sim_truth.solve()
-
-t_max = sim_truth.solution.t[-1]
-t = np.linspace(0, t_max, 100)
-truth_voltage = sim_truth.solution["Terminal voltage [V]"](t)
-
-other_pts = np.array([3, 5, 10, 20, 30, 40, 50, 60])
-solutions = np.zeros_like(other_pts)
-errors = np.linspace(0, 1, len(other_pts))
-
-for i, p in enumerate(other_pts):
-
-    model = pybamm.lithium_ion.DFN(options)
-    solver = pybamm.CasadiSolver(mode="fast")
-    sim = pybamm.Simulation(model, var_pts=pts(p), solver=solver)
-    sim.solve()
-    v = sim.solution["Terminal voltage [V]"](t)
-
-    model = pybamm.lithium_ion.SPMe(options)
-    solver = pybamm.CasadiSolver(mode="fast")
-    sim = pybamm.Simulation(model, var_pts=pts(p), solver=solver)
-    sim.solve()
-    v_spm = sim.solution["Terminal voltage [V]"](t)
-
-    errors[i] = np.sum((v - v_spm) ** 2) ** (1 / 2)
-
-plt.plot(other_pts, errors)
-plt.show()
-
-out = {"pts": other_pts, "errors": errors}
-pickle.dump(out, open("convergence_spme.p", "wb"))
+    plt.plot(other_pts, voltage_errors)
+    plt.plot(other_pts, c_s_n_errors)
+    plt.plot(other_pts, c_s_p_errors)
+    plt.plot(other_pts, phi_s_n_errors)
+    plt.legend("Voltage", "Negative particle concentration")
+    plt.show()
 
